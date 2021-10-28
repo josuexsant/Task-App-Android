023910c2216dd6f1a51a416d9511f4e23d827328
Josue Santamaria <josuexsanta@gmail.com>
2021-10-28 15:53:33 -0500

    Se agregaron:

--- app/src/main/java/com/example/task/Edit.java
+++ app/src/main/java/com/example/task/Edit.java
@@ -1,9 +1,14 @@
 package com.example.task;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Edit extends AppCompatActivity {
 
@@ -11,10 +16,24 @@ public class Edit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        Toolbar barraEdicion = findViewById(R.id.memuEditar);
+        setSupportActionBar(barraEdicion);
     }
     //Metodo para inicar el menu desplegabe
     public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu, menu);
+        getMenuInflater().inflate(R.menu.menu_editing, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.backtomain){
+            Intent Main = new Intent(this, MainActivity.class);
+            startActivity(Main);
+            Toast.makeText(this, "Guardado", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/task/MainActivity.java
+++ app/src/main/java/com/example/task/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.task;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //barra de herramientas
+        Toolbar barra = findViewById(R.id.toolbar);
+        setSupportActionBar(barra);
     }
 
     public void editNote(View view){
@@ -20,4 +28,23 @@ public class MainActivity extends AppCompatActivity {
         startActivity(editScreen);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.Info:
+                Intent infoScreen = new Intent(this, InfoActivity.class);
+                startActivity(infoScreen);
+            case R.id.Settings:
+                Toast.makeText(getApplicationContext(), "Proximamente", Toast.LENGTH_SHORT).show();
+            default:
+                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
